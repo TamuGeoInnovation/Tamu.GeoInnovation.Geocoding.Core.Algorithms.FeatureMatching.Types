a365d57f02f60f09a8a8ffad775bccc42c272365
dwgoldbe <daniel.goldberg@tamu.edu>
2015-06-18 15:40:52 -0500

    Initial Commit

---
+++ src/Main/Algorithms/FeatureMatchingMethods/FeatureMatcher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+
+namespace USC.GISResearchLab.Geocoding.Core.Algorithms.FeatureMatchingMethods
+{
+
+    [Flags]
+    public enum FeatureMatchTypes {
+        NoMatch = 1,
+        Exact = 2,
+        Relaxed = 4,
+        Substring = 8,
+        Soundex = 16,
+        Composite = 32,
+        Nearby = 64,
+        Unknown = 128,
+    };
+}
---
+++ src/Main/Algorithms/FeatureMatchingMethods/FeatureMatchingAmbiguousResult.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using USC.GISResearchLab.Common.Addresses;
+using USC.GISResearchLab.Common.Core.Geocoders.FeatureMatching;
+
+namespace USC.GISResearchLab.Geocoding.Core.Algorithms.FeatureMatchingMethods
+{
+    public enum FeatureMatchingAmbiguityType { None, ReferenceFeatureMultipleInstances, ReferenceFeatureOverlappingAddressRanges, InputAddressIncompleteMultiplePossibilities, InputAddressIncorrectMultiplePossibilities, SomethingElse, AmbiguityTypeNotComputable };
+}
